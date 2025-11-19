aa439642912ee81d55f1328f21b58539f591af2a
Juankamoray <imjuanky@gmail.com>
2025-11-19 21:15:47 +0100

    Porro

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -6,4 +6,14 @@ import { Component } from '@angular/core';
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
-export class Navbar {}
+export class Navbar {
+  isMenuOpen = false;
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  closeMenu() {
+    this.isMenuOpen = false;
+  }
+}
